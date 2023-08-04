16b31e39b79e590c1eb02c930b5f471bf921134a
osayenko0315 <126353984+osayenko0315@users.noreply.github.com>
2023-08-03 19:43:08 -0500

    added code

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,7 +2,7 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
 console.log('hit')
     console.log("event" + event)
     event.preventDefault();
